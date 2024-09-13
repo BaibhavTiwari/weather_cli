882368d5b308fcf7c6597cee751246beb25c6bdb
Baibhav <baibhavtiwari37@gmail.com>
2024-09-13 10:33:27 +0530

    somewhere in between

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use serde::Deserialize; 
+
+use colored::*; 
+#[derive(Deserialize,Debug)]
+
+struct WeatherResponse {
+    weather:Vec<Weather>,
+    main: Main,
+    wind: Wind,
+    name: String,
+}
+
+#[derive(Deserialize,Debug)]
+
+struct Weather{
+    description:String,
+}
+
+#[derive(Deserialize,Debug)]
+
+struct Main{
+    temp:f64,
+    humidity:f64,
+    pressure:f64
+}
+
+#[derive(Deserialize, Debug)]
+struct Wind {
+    speed: f64, // Wind speed in meters per second
+}
+
+fn get_weather_info(city:&str,country_code: &str, api_key: &str) -> 
+Result<WeatherResponse,reqwest::Error>{
+    let url
 }
